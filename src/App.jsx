96eafa366a0d91ffb53b9bec93c8ae009558ad21
hasanniloy122-rgb <hasanniloy122@gmail.com>
2026-03-14 13:49:46 +0600

    learned use from react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
 import Counter from './counter'
 import Batesman from './Batesman'
+import Users from './Users'
+import { Suspense } from 'react'
+import Friends from './Friends'
 
+const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+.then(res => res.json())
 
+const fetchFriends = async() => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  return res.json()
+}
 
 function App() {
+
+  const friendsPromise = fetchFriends()
 
   // function handleClick(){
   //   alert('I am clicked')
@@ -22,6 +33,14 @@ function App() {
   return (
     <>
       <h3>Get started</h3>
+      <Suspense fallback = {<h3>Loading...</h3>}>
+        <Users fetchUsers = {fetchUsers}></Users>
+      </Suspense>
+
+      <Suspense fallback={<h3>Friends are coming...</h3>}>
+        <Friends friendsPromise ={friendsPromise}></Friends>
+      </Suspense>
+
       <Counter></Counter>
       <Batesman></Batesman>
       
